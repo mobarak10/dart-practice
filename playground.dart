15b0ffbf108dbc84cc2c7c7d751ac5a9b1d24095
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2023-01-02 22:51:43 +0600

    using different kind of operator

--- playground.dart
+++ playground.dart
@@ -1,12 +1,40 @@
 import 'dart:io';
 main() {
-  // type conversion
+  // operator
 
-  // string to int
+  // mathematical
+  int num = 10 + 12;
+  num = num - 2;
+  print(num);
 
-  var one = int.parse('1');
-  assert(one == 1);
+  // modular
+  num = num % 5;
+  print(num);
 
-  var onePointOne = double.parse('1.1');
-  assert(onePointOne == 1.1);
+  //relational ==; ===; <=, >=;
+  if(num == 0) {
+    print("Zero");
+  }
+
+  num = 100;
+  num *= 2; // num = num * 2  
+  print(num);
+
+  //unary operator
+  ++num;
+  num++;
+  num += 1;
+  num -= 1;
+  print(num);
+
+  // logical && and logical ||
+  if(num > 200 && num < 203) {
+    print('200 to 202');
+  }
+
+  // != not equal
+  if(num != 100) {
+    print('Num is not equal to 100');
+  }
+
 }
