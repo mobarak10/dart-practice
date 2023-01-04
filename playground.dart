0248f48ab106fb5552b4f7981823741329607005
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2023-01-04 20:50:53 +0600

    practice break & continue

--- playground.dart
+++ playground.dart
@@ -1,40 +1,9 @@
 import 'dart:io';
+
 main() {
-  // operator
-
-  // mathematical
-  int num = 10 + 12;
-  num = num - 2;
-  print(num);
-
-  // modular
-  num = num % 5;
-  print(num);
-
-  //relational ==; ===; <=, >=;
-  if(num == 0) {
-    print("Zero");
+  // break & continue
+  for (var i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    print("Odd: $i");
   }
-
-  num = 100;
-  num *= 2; // num = num * 2  
-  print(num);
-
-  //unary operator
-  ++num;
-  num++;
-  num += 1;
-  num -= 1;
-  print(num);
-
-  // logical && and logical ||
-  if(num > 200 && num < 203) {
-    print('200 to 202');
-  }
-
-  // != not equal
-  if(num != 100) {
-    print('Num is not equal to 100');
-  }
-
 }
